e9b07ed18601264bd987f9127e65fb3d2570b32a
swandrn <159421702+swandrn@users.noreply.github.com>
2025-01-07 09:42:21 +0100

    change line number color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,6 +6,9 @@ return {
 		priority = 1000,
 		config = function()
 			vim.cmd.colorscheme "catppuccin-mocha"
+			vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'red' })
+			vim.api.nvim_set_hl(0, 'LineNr', { fg = 'red' })
+			vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'red' })
 		end
 	}
 }
