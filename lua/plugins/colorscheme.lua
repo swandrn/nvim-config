0200a99546af6d09c6052e34b13141a33a30b44e
swandrn <159421702+swandrn@users.noreply.github.com>
2025-01-09 16:36:24 +0100

    white line numbers

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,9 +6,9 @@ return {
 		priority = 1000,
 		config = function()
 			vim.cmd.colorscheme "catppuccin-mocha"
-			vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'red' })
-			vim.api.nvim_set_hl(0, 'LineNr', { fg = 'red' })
-			vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'red' })
+			vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'white' })
+			vim.api.nvim_set_hl(0, 'LineNr', { fg = 'white' })
+			vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'white' })
 		end
 	}
 }
