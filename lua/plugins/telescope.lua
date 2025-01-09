1bf17657ca7a67dbd41ae820c57204619ee529a0
swandrn <159421702+swandrn@users.noreply.github.com>
2025-01-09 21:12:46 +0100

    find ignored files with telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,13 @@ return {
 	config = function()
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+		vim.keymap.set('n', '<leader>fif', function()
+			builtin.find_files({
+				find_command = { 'rg', '--files', '--no-ignore' },
+				no_ignore = true,
+				desc = 'Telescope find files'
+			})
+		end)
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
